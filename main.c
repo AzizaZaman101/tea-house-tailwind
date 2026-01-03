e21982ed365a37642ea69002da9eb0ecef9c6266
AzizaZaman101 <azizazamansadia@gmail.com>
2026-01-03 13:40:31 +0600

    just the connections to the tailwind, google fonts, fontawesome have been done

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // This is my firstprogram!:)
+    /*This
+    is
+    my
+    first
+    one*/
+    printf("Hello World!\n");
+    printf("I am new!\n");
+    return 0;
+}
